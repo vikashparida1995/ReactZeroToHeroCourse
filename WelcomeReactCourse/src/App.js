ab0755cda9305265b4193ffb5c3d3b88f5c45c6e
Vikash Parida <Vikash.Parida@kwglobal.com>
2025-11-05 15:58:58 +0530

    create a app give a advice

--- WelcomeReactCourse/src/App.js
+++ WelcomeReactCourse/src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [advice , setAdvice] = useState("")
+  const [count , setCount]  = useState(0)
+
+  async function getAdvice(){
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json(res);
+    setAdvice(data.slip.advice)
+    setCount((c)=> c + 1 )
+
+  }
+
+  useEffect(function(){
+    getAdvice()
+  },[])
   return (
      <div className="ui container">
-     <h2> React Application</h2>
-     <button>Get Button</button>
+     <h2>{advice}</h2>
+     <button onClick={getAdvice}>Get Button</button>
+     <Message count={count} />
     </div>
   );
+}
+
+
+
+function Message(pops){
+  return (
+   <p> you have read <strong> {pops.count} </strong> pieces of advice</p>
+  )
 }
 
 export default App;
